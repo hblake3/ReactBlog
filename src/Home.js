5c42e4dd52c01d9062d42a749ec724dc0e699d94
Hunter <hntrblake@gmail.com>
2024-10-20 15:37:06 -0400

    setup click events

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Clicked on ', e);
+    }
+
+    const handleClickAgain = (name, e) =>{
+        console.log(`Hello, ${name}`, e.target);
+    }
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain('Yoshi', e)}>Click Me Again</button>
         </div>
      );
 }
