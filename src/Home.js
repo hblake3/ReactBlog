f33d54ff63491d7019979469780c54d1a430b31b
Hunter <hntrblake@gmail.com>
2024-10-20 16:04:36 -0400

    add useState hook onClick

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
+import { useState } from "react";
+
 const Home = () => {
+    const [name, setName] = useState('Mario');
+    const [age, setAge] = useState(36);
 
     const handleClick = (e) => {
-        console.log('Clicked on ', e);
+        setName(name === 'Mario' ? 'Luigi' : 'Mario');
+        setAge(name ==='Mario' ? 24 : 26);
     }
 
-    const handleClickAgain = (name, e) =>{
-        console.log(`Hello, ${name}`, e.target);
-    }
-    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain('Yoshi', e)}>Click Me Again</button>
         </div>
      );
 }
